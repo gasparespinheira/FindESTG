6cde68242c847bcc503bb132ae94bddc3f20a61c
rtorres76 <rutorres@ipvc.pt>
2023-04-06 22:23:49 +0100

    Commit DB 12 Base de Dados da FireBase a funfar tudo em ordem

--- app/src/main/java/ipvc/estg/findestg/MainActivity.kt
+++ app/src/main/java/ipvc/estg/findestg/MainActivity.kt
@@ -1,11 +1,38 @@
 package ipvc.estg.findestg
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Rui",
+            "last" to "Torres",
+            "born" to 2000
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+
     }
 }
