66c80e1a4a69ca098a525b306172ab9422c73610
j.o.pereira <j.o.pereira1>
2023-04-16 22:19:38 +0100

    segundo commit da home activity

--- app/src/main/java/ipvc/estg/findestg/MainActivity.kt
+++ app/src/main/java/ipvc/estg/findestg/MainActivity.kt
@@ -6,7 +6,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.database.*
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -14,7 +13,6 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private var db = Firebase.firestore
-    private var valor_botao: String? = null
     private lateinit var localizacaoList: ArrayList<Localizacao>
     private lateinit var localizacao_recycler_view: RecyclerView
 
@@ -27,6 +25,20 @@ class MainActivity : AppCompatActivity() {
 
         localizacaoList = arrayListOf()
 
+        getDataLocalizacoes()
+    }
+
+    fun getLocalizacaoSalas(view: View?) {
+
+    }
+    fun getLocalizacaoAuditorios(view: View) {
+
+    }
+    fun getLocalizacaoServicos(view: View) {
+
+    }
+
+    private fun getDataLocalizacoes() {
         db = FirebaseFirestore.getInstance()
         db.collection("localizacoes")
             .get()
@@ -34,38 +46,16 @@ class MainActivity : AppCompatActivity() {
                 if(!it.isEmpty)
                     for(data in it.documents) {
                         val localizacao: Localizacao? = data.toObject(Localizacao::class.java)
-                        if(localizacao != null) {
-                            if (valor_botao == "salas"){
-                                if (localizacao.tipo == "sala"){
-                                    localizacaoList.add(localizacao)
-                                }
-                            } else if (valor_botao == "auditorios"){
-                                if (localizacao.tipo == "auditorio"){
-                                    localizacaoList.add(localizacao)
-                                }
-                            } else if (valor_botao == "servicos"){
-                                if (localizacao.tipo == "servico"){
-                                    localizacaoList.add(localizacao)
-                                }
-                            }
+                        if (localizacao != null) {
+                            localizacaoList.add(localizacao)
                         }
                     }
-                    localizacao_recycler_view.adapter = LocalizacaoListAdapter(localizacaoList)
+                localizacao_recycler_view.adapter = LocalizacaoListAdapter(localizacaoList)
             }
 
             .addOnFailureListener {
                 Toast.makeText(this@MainActivity, it.toString(), Toast.LENGTH_LONG).show()
             }
-    }
-
-    fun getLocalizacaoSalas(view: View?) {
-        valor_botao = "salas"
-    }
-    fun getLocalizacaoAuditorios(view: View) {
-        valor_botao = "auditorios"
-    }
-    fun getLocalizacaoServicos(view: View) {
-        valor_botao = "servicos"
     }
 }
 
