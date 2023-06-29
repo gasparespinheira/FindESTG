6c9c4f387bc9fa258b86691e6b010a8b4291e935
Jota30200201 <101662254+Jota30200201@users.noreply.github.com>
2023-06-29 11:08:14 +0100

    MainActivity.kt código feito. Agora é só testar num telemóvel.

--- app/src/main/java/ipvc/estg/findestg/MainActivity.kt
+++ app/src/main/java/ipvc/estg/findestg/MainActivity.kt
@@ -2,10 +2,44 @@ package ipvc.estg.findestg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.Nullable
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var scanBtn: Button
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scanBtn = findViewById(R.id.scanner)
+        textView = findViewById(R.id.text)
+
+        scanBtn.setOnClickListener(View.OnClickListener {
+            val intentIntegrator = IntentIntegrator(this@MainActivity)
+            intentIntegrator.setOrientationLocked(true)
+            intentIntegrator.setPrompt("Scan a QR Code")
+            intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            intentIntegrator.initiateScan()
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, @Nullable data: Intent?) {
+        val intentResult: IntentResult? = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (intentResult != null) {
+            val contents: String? = intentResult.contents
+            if (contents != null) {
+                textView.setText(intentResult.contents)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
