12599e211707f5ccca509b1be795393db2f6d279
j.o.pereira <j.o.pereira1>
2023-06-30 18:30:53 +0100

    pagina inicial com a linha de butoes esconida

--- app/src/main/java/ipvc/estg/findestg/LocalizacaoListAdapter.kt
+++ app/src/main/java/ipvc/estg/findestg/LocalizacaoListAdapter.kt
@@ -3,17 +3,28 @@ package ipvc.estg.findestg
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class LocalizacaoListAdapter (val localizacao_list: ArrayList<Localizacao>):RecyclerView.Adapter<LocalizacaoViewHolder>(){
+class LocalizacaoListAdapter(private val localizacao_list: ArrayList<Localizacao>) :
+    RecyclerView.Adapter<LocalizacaoViewHolder>() {
+
+    private var selectedItemPosition = RecyclerView.NO_POSITION
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocalizacaoViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_localizacao_list, parent, false)
 
-        val itemView = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.item_localizacao_list, parent, false);
-        return LocalizacaoViewHolder(itemView)
+        return LocalizacaoViewHolder(itemView).apply {
+            itemView.setOnClickListener {
+                val previousSelectedPosition = selectedItemPosition
+                selectedItemPosition = adapterPosition
+
+                notifyItemChanged(previousSelectedPosition)
+                notifyItemChanged(selectedItemPosition)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
@@ -22,16 +33,26 @@ class LocalizacaoListAdapter (val localizacao_list: ArrayList<Localizacao>):Recy
 
     override fun onBindViewHolder(holder: LocalizacaoViewHolder, position: Int) {
         val currentLocalizacao = localizacao_list[position]
+        val isItemSelected = position == selectedItemPosition
 
         holder._descricao.text = currentLocalizacao.descricao
+
+        if (isItemSelected) {
+            holder._linhaOcultaLayout.visibility = View.VISIBLE
+        } else {
+            holder._linhaOcultaLayout.visibility = View.GONE
+        }
     }
 }
 
-class LocalizacaoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+class LocalizacaoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     val _descricao: TextView
+    val _linhaOcultaLayout: LinearLayout
 
     init {
         _descricao = itemView.findViewById(R.id.descricao_item)
+        _linhaOcultaLayout = itemView.findViewById(R.id.linha_oculta)
     }
 }
+
